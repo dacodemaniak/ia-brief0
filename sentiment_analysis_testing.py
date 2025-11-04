ddcce4839358cf94b320168f105fabcd704c7d82
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2025-11-04 12:22:35 +0100

    FIX (ignore) remove __pycache__ folder from tracking

--- sentiment_analysis_testing.py
+++ sentiment_analysis_testing.py
@@ -1,5 +1,5 @@
 from nltk.sentiment import SentimentIntensityAnalyzer
 
 sia = SentimentIntensityAnalyzer()
-sentiment = sia.polarity_scores("Bonjour")
+sentiment = sia.polarity_scores("Bonjour, je me sens plutôt bien aujourd'hui")
 print(sentiment)
